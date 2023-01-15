2bf3b094b12dd016f0a6a774e44b0c672c764307
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-16 04:36:19 +0900

    feat: add content to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,15 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer className="footer">
-      Footer
+      <h3 className="footer__title">EcoFarm | Экоферма по выращиванию микрозелени в Благовещенске</h3>
+      <div className="footer__content">
+        <p className="footer__copyright">&copy; Сергей Гринченко. 2023</p>
+        <ul className="footer__list">
+          <li className="footer__list-item"><a href="https://www.instagram.com/" className="footer__link link" target="_blank" rel="noopener noreferrer">Инстаграм</a></li>
+          <li className="footer__list-item"><a href="https://vk.com/" className="footer__link link" target="_blank" rel="noopener noreferrer">VK</a></li>
+          <li className="footer__list-item"><a href="https://web.telegram.org/" className="footer__link link" target="_blank" rel="noopener noreferrer">Telegram</a></li>
+        </ul>
+      </div>
     </footer>
   )
 }
