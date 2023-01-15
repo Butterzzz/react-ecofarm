62205f5f85d6fdcd2f1505c98ed52ba90f6e4ebe
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-16 04:59:16 +0900

    feat: add Blog component

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Blog.css'
+
+const Blog = () => {
+    return (
+        <section className="blog" id="blog">
+            <h2 className="blog__title">Отзывы</h2>
+        </section>
+    )
+}
+
+export default Blog
