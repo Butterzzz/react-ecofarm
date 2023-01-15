67b504873945424c5fb86522eabf423b8f924da6
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-16 02:52:26 +0900

    feat: add Logo component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Header.css'
+import Logo from '../../components/Logo/Logo'
 
 const Header = () => {
     return (
         <header className="header">
+            <Logo />
             Header
         </header>
     )
