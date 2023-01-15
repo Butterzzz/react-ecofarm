7f49e8291d077a80d8dffcac67c7fdff5b257837
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-16 04:59:47 +0900

    feat: add Reviews components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Main.css'
 import Catalog from '../Catalog/Catalog'
+import Blog from '../Blog/Blog'
 import Contacts from '../Contacts/Contacts'
+import Reviews from '../Reviews/Reviews'
 
 const Main = () => {
   return (
     <main className="content">
       <Catalog />
+      <Blog />
+      <Reviews />
       <Contacts />
     </main>
   )
---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Reviews.css'
+
+const Reviews = () => {
+    return (
+        <section className="reviews" id="reviews">
+            <h2 className="reviews__title">Отзывы</h2>
+        </section>
+    )
+}
+
+export default Reviews
