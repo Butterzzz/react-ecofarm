d6e7b9ba2e042bfa21e7f92c8c54b73db36b684f
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-16 04:49:31 +0900

    feat: add Contacts components

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contacts.css'
+
+const Contacts = () => {
+  return (
+    <section className="contacts" id="contacts">
+      <h2 className="contacts__title">Контакты</h2>
+    </section>
+  )
+}
+
+export default Contacts
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Main.css'
 import Catalog from '../Catalog/Catalog'
+import Contacts from '../Contacts/Contacts'
 
 const Main = () => {
   return (
     <main className="content">
       <Catalog />
+      <Contacts />
     </main>
   )
 }
