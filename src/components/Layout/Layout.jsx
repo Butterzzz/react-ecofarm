a86d6f17ee476d19942cfca0950fdbbf0daaffa9
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-19 03:24:19 +0900

    feat: add Layout component

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import './Layout.css'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+
+            <main className="content">
+                <Outlet />
+            </main>
+
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
