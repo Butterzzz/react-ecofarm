dc431e364879baba8765ac3bb24ba2caab5a09ce
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-22 23:53:25 +0900

    feat: add React Icons

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,18 +5,20 @@ import Layout from '../Layout/Layout'
 import Main from '../Main/Main'
 import Catalog from '../Catalog/Catalog'
 import NotFound from '../NotFound/NotFound'
-
+import { IconContext } from 'react-icons'
 
 const App = () => {
   return (
     <div className="page">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <IconContext.Provider value={{ className: "react-icons" }}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Main />} />
+            <Route path="catalog" element={<Catalog />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </IconContext.Provider>
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Hero.css'
 import promoImg from '../../images/promo.jpg'
+import { AiFillPlayCircle } from 'react-icons/ai'
 
 const Hero = () => {
     return (
@@ -12,7 +13,7 @@ const Hero = () => {
                     <p className="hero__subtitle">Свежая микрозелень с доставкой по всему городу</p>
                     <div className="hero__button-container">
                         <Link to="catalog" className="hero__button button">Каталог</Link>
-                        <button className="hero__button button">О производстве</button>
+                        <button className="hero__button button"><AiFillPlayCircle /> О производстве</button>
                     </div>
                 </div>
                 <img className="hero__image" src={promoImg} alt="Микрозелень" />
