068dd66a7ed9dbe738882525ce8a8326b02b8149
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-23 00:18:25 +0900

    feat: add CardList component

---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './CardList.css'
+import Card from '../Card/Card'
+// import cardsList from '../../data/cards'
+
+const CardList = () => {
+  return (
+    <section className="cards" aria-label="Карточки">
+      <ul className="cards__list">
+        <Card />
+        {/* {cardsList.map((card) => (
+          <Card
+            key={card._id}
+            card={card}
+          />
+        ))
+        } */}
+      </ul>
+    </section>
+  )
+}
+
+export default CardList
