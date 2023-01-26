ff260b07fc32acfb9090a15fab9924c7eaeb1c2a
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-26 22:36:56 +0900

    feat: add Preloader component

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CardList.css'
 import Card from '../Card/Card'
+import Preloader from '../Preloader/Preloader'
 import { cards } from '../../data/data'
 
 const CardList = () => {
   // const newCard = cards.filter(item => item.new === true);
   // console.log(newCard);
   
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => setIsLoading(false), 3000) // бутафория
+
   return (
     <section className="cards" aria-label="Карточки">
+      {isLoading && <Preloader />}
       <ul className="cards__list">
         {cards.map((card) => (
           <Card
