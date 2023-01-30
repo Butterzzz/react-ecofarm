b20494642e98dd6f89838450002b909383d2faa0
Grinchenko Sergei <butters.blaga@gmail.com>
2023-01-31 03:04:41 +0900

    refactor: Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Hero.css'
 import promoImg from '../../images/promo.jpg'
-import { BsPlayCircle} from 'react-icons/bs'
+// import { BsPlayCircle } from 'react-icons/bs'
 
 const Hero = ({ onClickAbout }) => {
     return (
@@ -13,8 +13,8 @@ const Hero = ({ onClickAbout }) => {
                     <p className="hero__subtitle">Свежая микрозелень с доставкой по всему городу</p>
                     <div className="hero__button-container">
                         <Link to="catalog" className="hero__button button">Каталог</Link>
-                        <button className="hero__button button" onClick={onClickAbout}>
-                            <BsPlayCircle className="hero__button_icon" />
+                        <button className="hero__button button " onClick={onClickAbout}>
+                            {/* <BsPlayCircle className="hero__button_icon" /> */}
                             О производстве
                         </button>
                     </div>
