d19bb8ad227a97ac21b05637b5eebce25c86339d
Grinchenko Sergei <butters.blaga@gmail.com>
2023-02-07 00:48:31 +0900

    fix: Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,33 +3,49 @@ import { Link } from 'react-router-dom'
 import './Card.css'
 
 const Card = ({ card, setOrder }) => {
-    // function handleClick() {
-    //     alert('По кнопке кликнули');
-    // }
+    const newPrice = (card.price * (1 - card.discount)).toFixed(0);
 
     return (
         <li className="cards__list-item">
             <article className="card">
+
                 <div className="card__chevron-container">
                     {card.isNew && <p className='card__chevron card__chevron_new'>Новинка</p>}
                     {card.isHit && <p className='card__chevron card__chevron_hit'>Хит продаж</p>}
                     {card.isSale && <p className='card__chevron card__chevron_sale'>Распродажа</p>}
                 </div>
-                <div className="card__image-container card__image-container_type_loading">
+
+                <div className="card__image-container">
                     <img className="card__image" src={card.image} alt={card.title} />
                 </div>
-                <h2 className="card__title">{card.title}</h2>
+
+                <h3 className="card__title">{card.title}</h3>
                 <p className="card__description">{card.about}</p>
-                <p className="card__price">{card.price} <s>{card.discount}</s></p>
-                <div className="card__button-wrapper">
-                    <Link to={`/catalog/${card.id}`} className="card__button button card__button_type_more">Подробнее</Link>
-                    <button className="card__button button card__button_type_add" type='button' onClick={() => setOrder({
-                        id: card.id,
-                        image: card.image,
-                        title: card.title,
-                        price: card.price,
-                    })}
-                    >Купить</button>
+
+                <div className="card__prices-container">
+                    <span className="card__price card__price_type_original">{newPrice} ₽</span>
+                    {card.discount > 0 && (
+                        <span className="card__price card__price_type_discount"><s>{card.price} ₽</s></span>
+                    )}
+                </div>
+
+                <div className="card__buttons-container">
+                    <Link
+                        to={`/catalog/${card.id}`}
+                        className="card__button button card__button_type_more">
+                        Подробнее
+                    </Link>
+                    <button
+                        className="card__button button card__button_type_buy"
+                        onClick={() => setOrder({
+                            id: card.id,
+                            image: card.image,
+                            title: card.title,
+                            price: newPrice,
+                            discount: card.discount,
+                        })}>
+                        Купить
+                    </button>
                 </div>
             </article>
         </li>
