4e36e04fade88a539b000b7a1ea2e1cc2b1cad12
Grinchenko Sergei <butters.blaga@gmail.com>
2023-02-12 02:03:14 +0900

    feat: add cart badge

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './UserBar.css'
 import { MdOutlineShoppingCart } from 'react-icons/md'
 
-export const UserBar = ({ onDrawerClick }) => {
+export const UserBar = ({ onDrawerClick, itemCount }) => {
 
     return (
         <div className="userBar">
             <button className="userBar__cart" type="button" onClick={onDrawerClick}>
                 <MdOutlineShoppingCart className="userBar__icon" />
-                <span className="userBar__text">Корзина</span>
+                <span class="userBar__badge">{itemCount}</span>
+                <span className="userBar__text link">Корзина</span>
             </button>
         </div>
     )
