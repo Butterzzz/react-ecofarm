f01822568ac08da40167e8f810cb5c2f8ba25d74
Grinchenko Sergei <butters.blaga@gmail.com>
2023-02-15 02:44:55 +0900

    style: fix Hero's style

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Hero.css'
-import promoImg from '../../images/promo.jpg'
-// import { BsPlayCircle } from 'react-icons/bs'
+import { BsPlayCircle } from 'react-icons/bs'
 
 const Hero = ({ onClickAbout }) => {
     return (
         <section className="hero">
-            <div className="hero__container">
-                <div className="hero__content">
-                    <h1 className="hero__title">Микрозелень в Благовещенске</h1>
-                    <p className="hero__subtitle">Свежая микрозелень с доставкой по всему городу</p>
-                    <div className="hero__button-container">
-                        <Link to="catalog" className="hero__button button">Каталог</Link>
-                        <button className="hero__button button " onClick={onClickAbout}>
-                            {/* <BsPlayCircle className="hero__button_icon" /> */}
-                            О производстве
-                        </button>
-                    </div>
-                </div>
-                <div className="hero__image-container">
-                    <img className="hero__image" src={promoImg} alt="Микрозелень" />
+            <div className="hero__container container">
+                <h1 className="hero__title">Микрозелень в Благовещенске</h1>
+                <p className="hero__subtitle">Свежая микрозелень с доставкой по всему городу</p>
+                <div className="hero__button-container">
+                    <Link
+                        to="catalog"
+                        className="hero__button hero__button_primary button"
+                    >
+                        Каталог
+                    </Link>
+                    <button
+                        className="hero__button hero__button_secondary button"
+                        onClick={onClickAbout}
+                    >
+                        <BsPlayCircle
+                            className="hero__button_icon"
+                        />
+                        О производстве
+                    </button>
                 </div>
             </div>
         </section>
