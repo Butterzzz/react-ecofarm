c498df034772db4177670c6beaf0f7bb96843d90
Grinchenko Sergei <butters.blaga@gmail.com>
2023-02-20 19:05:42 +0900

    feat: add responsive footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,9 +9,35 @@ const Footer = () => {
         <div className="footer__content">
           <p className="footer__copyright">&copy; Сергей Гринченко. 2023</p>
           <ul className="footer__list">
-            <li className="footer__list-item"><a href="https://www.instagram.com/" className="footer__link link" target="_blank" rel="noopener noreferrer">Инстаграм</a></li>
-            <li className="footer__list-item"><a href="https://vk.com/" className="footer__link link" target="_blank" rel="noopener noreferrer">VK</a></li>
-            <li className="footer__list-item"><a href="https://web.telegram.org/" className="footer__link link" target="_blank" rel="noopener noreferrer">Telegram</a></li>
+
+            <li className="footer__list-item">
+              <a href="https://www.instagram.com/"
+                className="footer__link link"
+                target="_blank"
+                rel="noopener noreferrer">
+                Инстаграм
+              </a>
+            </li>
+
+            <li className="footer__list-item">
+              <a href="https://vk.com/"
+                className="footer__link link"
+                target="_blank"
+                rel="noopener noreferrer">
+                VK
+              </a>
+            </li>
+
+            <li
+              className="footer__list-item">
+              <a href="https://web.telegram.org/"
+                className="footer__link link"
+                target="_blank"
+                rel="noopener noreferrer">
+                Telegram
+              </a>
+            </li>
+
           </ul>
         </div>
       </div>
